65e59f990fbf85050a49de75d4147b45805841dd
ryanzhangofficial <ryanzhangofficial@gmail.com>
2024-10-15 21:28:19 -0400

    Add base

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
